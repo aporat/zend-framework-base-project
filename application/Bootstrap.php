27fff30a1d1d214a41226e85ad4ad904e539dfe4
Adar Porat <adar.porat@gmail.com>
2012-02-20 23:08:53 -0500

    initial layout

---
+++ application/Bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+
+	protected function _initAutoload()
+	{
+		$autoloader = new Zend_Application_Module_Autoloader(array(
+				'namespace' => '',
+				'basePath'  => dirname(__FILE__),
+		));
+	
+		$autoloader = Zend_Loader_Autoloader::getInstance()
+		->registerNamespace('Application')
+		->registerNamespace('Services');
+	
+		return $autoloader;
+	}
+
+	protected function _initModules()
+	{
+		$front = Zend_Controller_Front::getInstance();
+		$front->addModuleDirectory(APPLICATION_PATH . '/modules');
+		
+		$frontController = Zend_Controller_Front::getInstance();
+	
+	}
+	
+}
+
